7ebbf4532cc6c72a850ecdc0bdf74ac4cddde1a0
so1218 <K024G0110@g.neec.ac.jp>
2025-06-07 10:59:19 +0900

    03_00完了

---
+++ MathUtils.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Structures.h"
+
+// 正射影ベクトルを求める関数
+Vector3 Project(const Vector3& v1, const Vector3& v2);
+// 最近接点を求める関数
+Vector3 CrosestPoint(const Vector3& point, const Segment& segment);
+// クロス積を求める関数
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
+float DistanceSquared(const Vector3& a, const Vector3& b);
+Vector3 Perpendicular(const Vector3& vector);
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
+
